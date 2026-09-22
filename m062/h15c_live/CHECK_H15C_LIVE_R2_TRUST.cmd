91cd8c55b03f7a238e1d9c02df79f7184d624f68
onish12 <onis.h.a.s.h.ak.ei.a3736@gmail.com>
2026-09-22 18:32:02 +0300

    Normalize H15C trust-check package path

--- m062/h15c_live/CHECK_H15C_LIVE_R2_TRUST.cmd
+++ m062/h15c_live/CHECK_H15C_LIVE_R2_TRUST.cmd
@@ -2,7 +2,11 @@
 setlocal
 cd /d "%~dp0"
 set "PHASER_PACKAGE=%~1"
-if not defined PHASER_PACKAGE set "PHASER_PACKAGE=%~dp0"
+if not defined PHASER_PACKAGE (
+  for %%I in ("%~dp0.") do set "PHASER_PACKAGE=%%~fI"
+) else (
+  for %%I in ("%PHASER_PACKAGE%") do set "PHASER_PACKAGE=%%~fI"
+)
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Check-H15cLivePackageTrust.ps1" -PackageRoot "%PHASER_PACKAGE%"
 set "PHASER_EXIT=%ERRORLEVEL%"
 echo.
