c6522daa8ede98a45029db5e33da0345ed69f37d
onish12 <onis.h.a.s.h.ak.ei.a3736@gmail.com>
2026-09-23 21:56:03 +0300

    Fix H15OR R2 alias-collision static test

--- tests/m0615_h15or_r2_noninteractive_tests.ps1
+++ tests/m0615_h15or_r2_noninteractive_tests.ps1
@@ -3,8 +3,8 @@ $root=Join-Path $PSScriptRoot '..'
 $r=Get-Content (Join-Path $root 'm062\h15or_r2\Run-H15orR2Recovery.ps1') -Raw
 $c=Get-Content (Join-Path $root 'm062\h15or_r2\RUN_H15OR_R2_RECOVERY.cmd') -Raw
 if($r.IndexOf('Set-Content',[StringComparison]::OrdinalIgnoreCase)-ge0){throw 'H15OR_R2_SET_CONTENT_FORBIDDEN'}
-if($r -match '(?im)^\\s*function\\s+SC\\s*\\('){throw 'H15OR_R2_SC_ALIAS_COLLISION_FORBIDDEN'}
-if($r -match '(?im)(^|[;{])\\s*SC\\s+@\\('){throw 'H15OR_R2_SC_ALIAS_INVOCATION_FORBIDDEN'}
+if($r.IndexOf('function SC(',[StringComparison]::OrdinalIgnoreCase)-ge0){throw 'H15OR_R2_SC_ALIAS_COLLISION_FORBIDDEN'}
+if($r.IndexOf('SC @(',[StringComparison]::OrdinalIgnoreCase)-ge0){throw 'H15OR_R2_SC_ALIAS_INVOCATION_FORBIDDEN'}
 if($r.IndexOf('function InvokeScExe(',[StringComparison]::Ordinal)-lt0){throw 'H15OR_R2_INVOKE_SC_EXE_MISSING'}
 foreach($x in @('RUNNER=H15OR_R2_NONINTERACTIVE_RECOVERY','RUNNER_BUILD=h15or-r2.1-sc-alias-fix-20260923','H15OR_R2_RUNNER_SELF_HASH_MISMATCH',"Purpose -cne 'H15OR_R2_READONLY_RECOVERY_PACKAGE'",'Phaser360H15orR2')){if($r.IndexOf($x,[StringComparison]::OrdinalIgnoreCase)-lt0){throw "H15OR_R2_RUNNER_MISSING:$x"}}
 foreach($x in @('-NonInteractive','Run-H15orR2Recovery.ps1','RUNNER_BUILD=h15or-r2.1-sc-alias-fix-20260923')){if($c.IndexOf($x,[StringComparison]::OrdinalIgnoreCase)-lt0){throw "H15OR_R2_CMD_MISSING:$x"}}
